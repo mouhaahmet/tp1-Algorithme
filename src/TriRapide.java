1cce87f1dd27f6566fab7118260a37b82b6ce03d
mouhaahmet <mouhamadoulaminecisse98@gmail.com>
2020-02-05 22:47:58 +0100

    veille cours

--- src/TriRapide.java
+++ src/TriRapide.java
@@ -1,2 +1,30 @@
 public class TriRapide {
+    static int[] triRapide(int[] A, int p, int r) {
+        if (p < r) {
+            int q = partition(A, p, r);
+            triRapide(A, p, q - 1);
+            triRapide(A, q + 1, r);
+        }
+
+        return A;
+    }
+
+    private static int partition(int[] A, int p, int r) {
+        int x = A[r];
+        int i = p - 1;
+        for (int j = p; j < r; j++) {
+            if (A[j] < x) {
+                i = i + 1;
+                int temp = A[i];
+                A[i] = A[j];
+                A[j] = temp;
+            }
+        }
+        int temp = A[i + 1];
+        A[i + 1] = A[r];
+        A[r] = temp;
+        return i + 1;
+    }
 }
+
+
